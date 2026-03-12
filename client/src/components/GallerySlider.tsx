9c6ee3b9ec2cb24aacff5dbe3c779843356e6f73
babinicz <babinicz@onet.pl>
2026-03-12 20:35:17 +0100

    GallerySlider

--- client/src/components/GallerySlider.tsx
+++ client/src/components/GallerySlider.tsx
@@ -163,7 +163,7 @@ const GallerySlider = ({ images, title }: GallerySliderProps) => {
                     />
                     <div
                       className={`absolute bottom-0 left-0 ${
-                        image.type === "before" ? "bg-red-700" : "bg-green-700"
+                        image.type === "before" ? "bg-red-950" : "bg-green-950"
                       } text-white py-1 px-4 rounded-tr-lg font-medium`}
                     >
                       {image.type === "before"
